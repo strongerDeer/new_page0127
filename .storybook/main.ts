7e5eedb91956633f77e144a65b5d903fdc370c4a
strongerDeer <stronger_deer@naver.com>
2025-01-19 21:19:58 +0900

    📝 Edit: next.js turbopack - storybook 충돌 문제 해결

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,41 +1,24 @@
 import type { StorybookConfig } from '@storybook/nextjs';
-import path from 'path';
+import { VanillaExtractPlugin } from '@vanilla-extract/webpack-plugin';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-onboarding',
+    '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@chromatic-com/storybook',
     '@storybook/addon-interactions',
   ],
   framework: {
     name: '@storybook/nextjs',
     options: {},
   },
-  staticDirs: ['../public'],
-  docs: {
-    autodocs: 'tag',
-  },
-  webpackFinal: (config) => {
-    // config가 null이 아님을 확인
-    if (config.resolve) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        '@styles': path.resolve(__dirname, '../src/shared/styles'),
-        '@': path.resolve(__dirname, '../src'),
-      };
-    } else {
-      // resolve 객체가 없는 경우 생성
-      config.resolve = {
-        alias: {
-          '@styles': path.resolve(__dirname, '../src/shared/styles'),
-          '@': path.resolve(__dirname, '../src'),
-        },
-        fallback: {},
-      };
-    }
+  webpackFinal: async (config) => {
+    // vanilla-extract 설정
+    config.plugins = config.plugins || [];
+    config.plugins.push(new VanillaExtractPlugin());
+
     return config;
   },
 };
+
 export default config;
