930f0cf1a15963ad8203758f37574a85dbc49aeb
strongerDeer <stronger_deer@naver.com>
2025-01-20 19:15:10 +0900

    📝 Edit: tag 옵션으로 변경, div 기본 설정

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -1,12 +1,12 @@
 import styles from './Container.module.scss';
 
 export default function Container({
-  tag,
+  tag = 'div',
   children,
 }: {
-  tag: 'div' | 'main' | 'section' | 'article';
+  tag?: 'div' | 'main' | 'section' | 'article';
   children: React.ReactNode;
 }) {
-  const TagName = tag || 'div';
+  const TagName = tag;
   return <TagName className={styles.container}>{children}</TagName>;
 }
