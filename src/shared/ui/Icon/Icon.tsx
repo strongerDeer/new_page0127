dcef9a93a7b3284608fc55e1adad0109fd4c4cea
strongerDeer <stronger_deer@naver.com>
2025-01-23 22:55:32 +0900

    🌱 Add: 아이콘 추가

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,10 +1,15 @@
 import { FaGoogle } from 'react-icons/fa';
 import { AiOutlineHeart } from 'react-icons/ai';
+import { LuCircleCheck, LuCircleAlert, LuImage, LuX } from 'react-icons/lu';
 import React from 'react';
 
 export const ICONS = {
   google: <FaGoogle />,
   like: <AiOutlineHeart />,
+  error: <LuCircleAlert />,
+  check: <LuCircleCheck />,
+  image: <LuImage />,
+  close: <LuX />,
 } as const;
 
 export type IconType = keyof typeof ICONS;
