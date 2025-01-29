cb9cd133da818a36c35ef46b897740ee8886ce2f
strongerDeer <stronger_deer@naver.com>
2025-01-29 21:27:56 +0900

    🔥 Remove: 불필요한 코드 삭제

--- src/app/(book)/search/layout.tsx
+++ src/app/(book)/search/layout.tsx
@@ -1,11 +1,8 @@
-import { Suspense } from 'react';
-
 export default function Layout({ children }: { children: React.ReactNode }) {
-  // useSearchParams, suspense 경계 필요
   return (
-    <Suspense>
+    <>
       <div>검색input 영역</div>
       {children}
-    </Suspense>
+    </>
   );
 }
