a53a2964cb84d55dee52102daa2ba6d437e06fd4
strongerDeer <stronger_deer@naver.com>
2025-02-03 15:43:56 +0900

    📝 Edit: props 및 타입 수정

--- src/shared/ui/Container/Container.tsx
+++ src/shared/ui/Container/Container.tsx
@@ -1,17 +1,20 @@
 import styles from './Container.module.scss';
 
+interface ContainerProps extends React.HTMLAttributes<HTMLElement> {
+  tag?: 'div' | 'main' | 'section' | 'article';
+  children: React.ReactNode;
+  center?: boolean;
+}
+
 export default function Container({
   tag = 'div',
   children,
   center,
-}: {
-  tag?: 'div' | 'main' | 'section' | 'article';
-  children: React.ReactNode;
-  center?: boolean;
-}) {
+  ...rest
+}: ContainerProps) {
   const TagName = tag;
   return (
-    <TagName className={`${styles.container} ${center && 'center'}`}>
+    <TagName className={`${styles.container} ${center && 'center'}`} {...rest}>
       {children}
     </TagName>
   );
